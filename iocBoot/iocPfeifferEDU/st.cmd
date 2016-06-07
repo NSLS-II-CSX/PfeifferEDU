97850ccbc1a817d2894b421851a4585f74b006d6
SoftIOC <swilkins@bnl.gov>
2016-06-07 13:35:12 -0400

    Updated to add pump

--- iocBoot/iocPfeifferEDU/st.cmd
+++ iocBoot/iocPfeifferEDU/st.cmd
@@ -15,9 +15,13 @@ PfeifferEDU_registerRecordDeviceDriver(pdbbase)
 drvAsynIPPortConfigure("PUMP1","10.23.2.63:4016",0,0,0)
 #asynSetTraceMask("PUMP1", 0, 0x9)
 #asynSetTraceIOMask("PUMP1", 0, 0x2)
+drvAsynIPPortConfigure("PUMP2","10.23.2.63:4015",0,0,0)
+#asynSetTraceMask("PUMP2", 0, 0x9)
+#asynSetTraceIOMask("PUMP2", 0, 0x2)
 
 ## Load record instances
 dbLoadRecords("db/PfeifferEDU_TC110.db","Sys=XF:23ID1-VA,Dev={Dif:LoadLck-TMP:1},ID=001,PORT=PUMP1,SCAN=2 second")
+dbLoadRecords("db/PfeifferEDU_TC110.db","Sys=XF:23ID1-VA,Dev={Dif-TMP:2},ID=001,PORT=PUMP2,SCAN=2 second")
 
 iocInit()
 
