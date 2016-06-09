8e503fd244bdefd0ade4bf20b685bc44430bdd0a
Stuart B. Wilkins <swilkins@bnl.gov>
2016-06-09 16:15:35 -0400

    Added CSX-2

--- iocBoot/iocPfeifferEDU/st.cmd
+++ iocBoot/iocPfeifferEDU/st.cmd
@@ -18,10 +18,14 @@ drvAsynIPPortConfigure("PUMP1","10.23.2.63:4016",0,0,0)
 drvAsynIPPortConfigure("PUMP2","10.23.2.63:4015",0,0,0)
 #asynSetTraceMask("PUMP2", 0, 0x9)
 #asynSetTraceIOMask("PUMP2", 0, 0x2)
+drvAsynIPPortConfigure("PUMP3","10.23.2.65:4016",0,0,0)
+#asynSetTraceMask("PUMP3", 0, 0x9)
+#asynSetTraceIOMask("PUMP3", 0, 0x2)
 
 ## Load record instances
 dbLoadRecords("db/PfeifferEDU_TC110.db","Sys=XF:23ID1-VA,Dev={Dif:LoadLck-TMP:1},ID=001,PORT=PUMP1,SCAN=2 second")
 dbLoadRecords("db/PfeifferEDU_TC400.db","Sys=XF:23ID1-VA,Dev={Dif-TMP:2},ID=001,PORT=PUMP2,SCAN=2 second")
+dbLoadRecords("db/PfeifferEDU_TC110.db","Sys=XF:23ID2-VA,Dev={IOXAS:LoadLck-TMP:1},ID=001,PORT=PUMP3,SCAN=2 second")
 
 iocInit()
 
