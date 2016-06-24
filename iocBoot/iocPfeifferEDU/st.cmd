5e5e667e5485f09d67f0b71ef19b439ee14ad5a4
SoftIOC <swilkins@bnl.gov>
2016-06-24 10:52:56 -0400

    Updates with venting

--- iocBoot/iocPfeifferEDU/st.cmd
+++ iocBoot/iocPfeifferEDU/st.cmd
@@ -21,12 +21,15 @@ drvAsynIPPortConfigure("PUMP2","10.23.2.63:4015",0,0,0)
 drvAsynIPPortConfigure("PUMP3","10.23.2.65:4016",0,0,0)
 #asynSetTraceMask("PUMP3", 0, 0x9)
 #asynSetTraceIOMask("PUMP3", 0, 0x2)
+drvAsynIPPortConfigure("PUMP4","10.23.2.65:4015",0,0,0)
+#asynSetTraceMask("PUMP4", 0, 0x9)
+#asynSetTraceIOMask("PUMP4", 0, 0x2)
 
 ## Load record instances
 dbLoadRecords("db/PfeifferEDU_TC110.db","Sys=XF:23ID1-VA,Dev={Dif:LoadLck-TMP:1},ID=001,PORT=PUMP1,SCAN=2 second")
 dbLoadRecords("db/PfeifferEDU_TC400.db","Sys=XF:23ID1-VA,Dev={Dif-TMP:2},ID=001,PORT=PUMP2,SCAN=2 second")
 dbLoadRecords("db/PfeifferEDU_TC110.db","Sys=XF:23ID2-VA,Dev={IOXAS:LoadLck-TMP:1},ID=001,PORT=PUMP3,SCAN=2 second")
-
+dbLoadRecords("db/PfeifferEDU_TC110.db","Sys=XF:23ID2-VA,Dev={APPES:LoadLck-TMP:1},ID=001,PORT=PUMP4,SCAN=2 second")
 iocInit()
 
 dbl > $(TOP)/records.dbl
